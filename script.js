a0d972f54e055bbff366ef31409992b7a344ad15
Nisharga-Nirjan <nirjannisharga@gmail.com>
2024-07-05 03:17:12 +0600

    fix: toggle collapse when touching outside

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
+
   menu.classList.toggle("open");
   icon.classList.toggle("open");
-}
 
+  // When touching outside while collapsed
+  document.addEventListener('click', function(event) {
+    if (!menu.contains(event.target) && !icon.contains(event.target)) {
+      if (menu.classList.contains('open')) {
+        menu.classList.remove('open');
+        icon.classList.remove('open');
+      }
+    }
+  });
+}
